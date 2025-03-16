3b3d987b5d6ae9d481873de64fb971f08761b802
sindishima <sshima19@epoka.edu.al>
2025-03-16 15:18:25 +0100

    feat

--- RouteAnalyzer/src/main/kotlin/data/Waypoint.kt
+++
@@ -1,9 +0,0 @@
-package data
-
-
-data class Waypoint(val timestamp : Double, val longitude : Double, val latitude : Double){
-//    private fun calculateDistance(position: GlobalCoordinates): Double {
-//        val curve = calculator.calculateGeodeticCurve(ellipsoidRef, this.position, position)
-//        return curve.ellipsoidalDistance / 1000
-//    }
-}
